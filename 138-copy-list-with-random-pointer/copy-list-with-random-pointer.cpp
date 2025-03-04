9dd38191d7e0d12d2f32ff97a55f19a4bdfa4547
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2025-03-04 22:48:09 +0530

    Time: 7 ms (53.34%) | Memory: 15.2 MB (49.53%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -15,7 +15,7 @@ public:
 */
 
 class Solution {
-    void Insertattail(Node* &head,Node* &tail,int d){
+        void Insertattail(Node* &head,Node* &tail,int d){
         Node* temp = new Node(d);
         if(head == NULL){
             head = temp;
@@ -29,57 +29,35 @@ class Solution {
             tail = temp;
         }
     }
-
 public:
     Node* copyRandomList(Node* head) {
-        Node* temp = head;
         Node* clonehead = NULL;
         Node* clonetail = NULL;
 
-        // Step1- Create a Clone List
+        Node* temp = head;
         while(temp!=NULL){
             Insertattail(clonehead,clonetail,temp->val);
             temp = temp->next;
         }
 
-        // Step 2- Create  a relation between original node and clone node
-        Node* ognode = head;
-        Node* clnode = clonehead;
-
-        while(ognode!=NULL ){
-            Node* ptr1 = ognode->next;
-            ognode->next = clnode;
-            ognode = ptr1;
-
-            Node* ptr2 = clnode->next;
-            clnode->next = ognode;
-            clnode = ptr2;
-        }
-
-        //step3- make pointer of         
         temp = head;
-        while(temp!=NULL){
-            if(temp->next!=NULL){
-                temp->next->random = temp->random ? temp->random->next : temp->random;
-            }
-            temp = temp->next->next;
-
-        }
-        //step  4 - revert;
-        ognode = head;
-        clnode = clonehead;
-
-        while(ognode!=NULL ){
-           ognode->next = clnode->next;
-           ognode = ognode->next;
-           if(ognode!=NULL){
-            clnode->next = ognode->next;
-           }
-           clnode =clnode->next;
+        Node* temp2 = clonehead;
+        unordered_map<Node*,Node*> oldtonew;
+        while(temp!=NULL && temp2!=NULL){
+            oldtonew[temp]=temp2;
+            temp = temp->next;
+            temp2 = temp2->next;
         }
 
+        temp = head;
+        temp2 = clonehead;
+        while(temp!=NULL && temp2!=NULL){
+            temp2->random=oldtonew[temp->random];
+            temp = temp->next;
+            temp2 = temp2->next;
+
+        }
         return clonehead;
-
 
         
     }
