c292aad98f79bca41406801f819d13f7b275e4ef
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2025-07-06 21:43:29 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (88.66%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,31 +1,33 @@
 class Solution {
+    bool dfs(int node, vector<vector<int>>& graph, vector<int> &col, int color) {
+        col[node] = color;
+
+        for(auto nbr : graph[node]) {
+            if(col[nbr] == -1) {
+                if(dfs(nbr, graph, col, 1 - color) == false)
+                    return false;
+            }
+            else if(col[nbr] == col[node]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        queue<int> q;
-        vector<int> col(n,-1);
-        for(int i =0;i<n;i++){
-            queue<int> q;
-            if(col[i]==-1){
-            col[i]=0;
-            q.push(i);
-            }
-            while(!q.empty()){
-                int top = q.front();
-                q.pop();
-                for(auto nbr : graph[top]){
-                    if(col[nbr]==-1){
-                        col[nbr]=1-col[top];
-                        q.push(nbr);
-                    }
-                    if(col[nbr]==col[top]){
-                        return false;
-                    }
-                }
-            }
+        vector<int> col(n, -1);
 
+        for(int i = 0; i < n; i++) {
+            if(col[i] == -1) {
+                bool isValid = dfs(i, graph, col, 0);
+                if(isValid == false)  // bina ! ke
+                    return false;
+            }
         }
+
         return true;
-        
     }
 };
