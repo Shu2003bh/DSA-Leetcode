15b242d82fb465c61c2ad97fa244a3d7fda067e7
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2025-07-19 15:47:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (76.25%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int solvetab(vector<int> &nums){
+        int n = nums.size();
+       
+       // vector<int> dp(n+1,0) 
+       int prev2 = 0;
+       int prev1 = nums[0];
+       for(int i =1;i<n;i++){
+        int inc = prev2+nums[i];
+        int exc = prev1+0;
+
+        int ans = max(inc,exc);
+        prev2 = prev1;
+        prev1 = ans;
+
+       }
+       return prev1;
+    }
+
+   
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> first,second;
+        if(n==1){
+            return nums[0];
+        }
+
+        for(int i =0;i<n;i++){
+            if(i!=n-1){
+                first.push_back(nums[i]);
+            }
+            if(i!=0){
+                second.push_back(nums[i]);
+            }
+        }
+        return max(solvetab(first),solvetab(second));
+        
+    }
+};
