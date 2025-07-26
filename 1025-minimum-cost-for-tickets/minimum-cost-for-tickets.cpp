1e5e8fee6bca8b668a6a99c7fa945a32d66238f1
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2025-07-26 15:58:25 +0530

    Time: 1 ms (35.44%) | Memory: 14.1 MB (23.98%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -70,12 +70,37 @@ public:
         }
         return dp[0];
     }
+    int solveopt(vector<int>& days, vector<int>& costs){
+        int ans =0;
+         queue<pair<int,int>> month;
+         queue<pair<int,int>> week;
+
+         for(auto day:days){
+
+            while(!month.empty() && month.front().first+30<=day){
+                month.pop();
+            }
+
+             while(!week.empty() && week.front().first+7<=day){
+                week.pop();
+            }
+
+            month.push({day,ans+costs[2]});
+            week.push({day,ans+costs[1]});
+
+            ans = min(ans+costs[0],min(month.front().second,week.front().second));
+
+         }
+         return ans;
+
+    }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
         vector<int> dp(n+1,-1);
         // return solve(days,costs,365,0);
         // return solvemem(days,costs,n,0,dp);
-        return solvetab(days,costs,n);
+        // return solvetab(days,costs,n);
+        return solveopt(days,costs);
         
     }
 };
