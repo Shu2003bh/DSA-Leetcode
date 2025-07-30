c725a426ea8879339dd486f80c545ee86c74212f
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2025-07-30 13:54:37 +0530

    Time: 7 ms (5.12%) | Memory: 12.4 MB (5.26%) - LeetSync

---
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int solvemem(vector<int>& v,int i ,int j,vector<vector<int>> &dp){
+          if(i+1 == j ){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int ans = INT_MAX;
+        for(int k =i+1;k<j;k++){
+            ans = min({ans,v[i]*v[j]*v[k]+solvemem(v,i,k,dp)+solvemem(v,k,j,dp)});
+            dp[i][j]=ans;
+        }
+        return dp[i][j];
+
+    }
+    int solve(vector<int>& v,int i ,int j){
+        if(i+1 == j ){
+            return 0;
+        }
+        int ans = INT_MAX;
+        for(int k =i+1;k<j;k++){
+            ans = min({ans,v[i]*v[j]*v[k]+solve(v,i,k)+solve(v,k,j)});
+        }
+        return ans;
+
+    }
+    int minScoreTriangulation(vector<int>& values) {
+        int n = values.size();
+        // return solve(values,0,n-1);
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return solvemem(values,0,n-1,dp);
+        
+    }
+};
