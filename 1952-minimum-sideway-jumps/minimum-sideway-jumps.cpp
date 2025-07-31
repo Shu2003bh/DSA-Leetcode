33189c4e6584d15d1769529c446f3878f5e587d2
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2025-07-31 22:37:00 +0530

    Time: 82 ms (46.50%) | Memory: 234.1 MB (70.35%) - LeetSync

--- 1952-minimum-sideway-jumps/minimum-sideway-jumps.cpp
+++ 1952-minimum-sideway-jumps/minimum-sideway-jumps.cpp
@@ -46,10 +46,73 @@ public:
         
 
     }
+    int solveTab(vector<int>& obstacles){
+        int n = obstacles.size();
+        vector<vector<int>> dp(4,vector<int>(n+1,INT_MAX));
+
+        dp[0][n]=0;
+        dp[1][n]=0;
+        dp[2][n]=0;
+        dp[3][n]=0;
+
+        for(int currpos = n-1;currpos>=0;currpos--){
+            for(int currlane = 1;currlane<=3;currlane++){
+                 if(obstacles[currpos+1]!=currlane){
+             dp[currlane][currpos] = dp[currlane][currpos+1];
+
+        }
+        else{
+            int ans = INT_MAX;
+            for(int i =1;i<=3;i++){
+                if(currlane !=i && obstacles[currpos]!=i){
+                    ans = min(ans,1+dp[i][currpos+1]);
+                }
+            dp[currlane][currpos] = ans;
+            }
+        }
+
+            }
+        }
+        return min({dp[2][0],1+dp[1][0],1+dp[3][0]});
+
+    }
+    int solveSO(vector<int>& obstacles){
+    int n = obstacles.size()-1;
+    vector<int> curr(4, INT_MAX);
+    vector<int> next(4, INT_MAX);
+
+    next[0] = 0;
+    next[1] = 0;
+    next[2] = 0;
+    next[3] = 0;
+
+    for (int currpos = n - 1; currpos >= 0; currpos--) {
+        for (int currlane = 1; currlane <= 3; currlane++) {
+            if (obstacles[currpos + 1] != currlane) {
+                curr[currlane] = next[currlane];
+            } else {
+                int ans = INT_MAX;
+                for (int i = 1; i <= 3; i++) {
+                    if (currlane != i && obstacles[currpos] != i) {
+                        ans = min(ans, 1 + next[i]);
+                    }
+                }
+                curr[currlane] = ans;  // fix: update curr, not next
+            }
+        }
+        next = curr;  // fix: update after inner loop
+    }
+
+    // Starting from lane 2, position 0
+    return min({next[2], 1 + next[1], 1 + next[3]});
+}
+
     int minSideJumps(vector<int>& obstacles) {
         int n = obstacles.size();
         vector<vector<int>> dp(4,vector<int>(n-1,-1));
-        return solvemem(obstacles,2,0,dp);
+        // return solvemem(obstacles,2,0,dp);
+        // return solveTab(obstacles);
+        return solveSO(obstacles);
         // return solve(obstacles,2,0);
         
     }
