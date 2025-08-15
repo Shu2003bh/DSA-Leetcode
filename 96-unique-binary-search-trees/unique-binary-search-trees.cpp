fdd596ee15843680bf04f0796ba9713683e0fe74
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2025-08-15 16:56:57 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (61.78%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -13,10 +13,25 @@ class Solution {
         }
        return dp[n] = ans;
     }
+    int solvetab(int number){
+        vector<int> dp(number+1,0);
+        dp[0]=1;
+        dp[1]=1;
+
+        for(int n =2;n<=number;n++){
+            for(int i =1;i<=n;i++){
+            dp[n] += dp[i-1]*dp[n-i];
+        }
+        }
+        return dp[number];
+
+
+    }
 public:
     int numTrees(int n) {
-        vector<int> dp(n+1,-1);
-        return solvemem(n,dp);
+        // vector<int> dp(n+1,-1);
+        // return solvemem(n,dp);
+        return solvetab(n);
         
     }
 };
