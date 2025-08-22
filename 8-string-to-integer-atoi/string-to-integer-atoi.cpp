34b42744a769b6d9c68f45a87309da2e165d03d1
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2025-08-22 22:46:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (56.11%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -2,36 +2,35 @@ class Solution {
 public:
     int myAtoi(string s) {
         int ans = 0;
-        int sign = 1;
         int i = 0;
-        // WhiteSpace
-        while (s[i] == ' ') {
+        int sign = 1;
+        int temp;
+       while(s[i]==' '){
             i++;
         }
-        // Signedness
-        if (s[i] == '-') {
-            sign = -1;
-            i++;
-        }else if (i < s.size() && s[i] == '+') {
+        if(s[i]=='-'){
+            sign =-1;
             i++;
         }
-        // Zero's
-        while (s[i] == '0') {
+        else if(i<s.size() && s[i]=='+'){
             i++;
         }
-        // Converting
-        while (i < s.size() && isdigit(s[i])) {
-            int temp = s[i] - '0';
-            // OverFlow and Underflow
-            if (ans > INT_MAX / 10 ||
-                (ans == INT_MAX / 10 && temp > INT_MAX % 10)) {
-                return sign == 1 ? INT_MAX : INT_MIN;
+        while(s[i]=='0'){
+            i++;
+        }
+
+        while(i<s.size() && isdigit(s[i])){
+            temp = s[i]-'0';
+
+            if(ans > INT_MAX/10 || ans==INT_MAX/10  && temp >INT_MAX%10){
+                return sign ==1 ? INT_MAX : INT_MIN;
             }
-
-            ans = ans * 10 + temp;
+            ans = ans*10+temp;
             i++;
         }
-
         return sign * ans;
+
+
+        
     }
 };
