bb017a3eaa98697e64abc86987b31b1608143964
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2025-08-22 21:19:02 +0530

    Time: 30 ms (37.75%) | Memory: 27.3 MB (77.76%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -21,14 +21,30 @@ public:
 
     }
 
-    // int solvetab(string &a, string &b){
-    //     vector<vector<int>> dp(text1.size(),vector<int>(text2.size(),-1));
+    int solvetab(string &a, string &b){
+        vector<vector<int>> dp(a.size()+1,vector<int>(b.size()+1,0));
+        
+        for(int i = a.size()-1;i>=0;i--){
+            for(int j =b.size()-1;j>=0;j--){
+                int ans = 0;
+        if(a[i]==b[j]){
+            ans = 1+dp[i+1][j+1];
+        }
+        else{
+            ans = max(dp[i][j+1],dp[i+1][j]);
+        }
+         dp[i][j] = ans;
 
-    // }
+            }
+        }
+        return dp[0][0];
+
+    }
 
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>> dp(text1.size()+1,vector<int>(text2.size()+1,-1));
-        return solvemem(0,0,text1,text2,dp);
+        // vector<vector<int>> dp(text1.size()+1,vector<int>(text2.size()+1,-1));
+        // return solvemem(0,0,text1,text2,dp);
+        return solvetab(text1,text2);
         
     }
 };
