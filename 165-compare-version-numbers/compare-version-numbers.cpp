1225972d6dabfbbec1a04adfb39432c57abcd31a
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2025-08-24 12:35:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (48.85%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int n = version1.size();
+        int m = version2.size();
+            int x1 = 0;
+            int x2 = 0;
+        for(int i =0,j=0;i < n || j < m;i++,j++){
+            while(i<n && version1[i]!='.'){
+                x1 = 10*x1+(version1[i++]-'0');
+            }
+            while(j<m && version2[j]!='.'){
+                x2 = 10*x2+(version2[j++]-'0');
+            }
+
+            if(x1<x2){
+                return -1;
+            }
+            else if(x1>x2){
+                return 1;
+            }
+            x1 =0;
+            x2 = 0;
+
+            
+        }
+        return 0;
+        
+    }
+};
+// class Solution {
+// public:
+//     static int compareVersion(string& v1, string& v2) {
+//         const int n1=v1.size(), n2=v2.size();
+//         int x1=0, x2=0;
+//         for(int i=0, j=0; i<n1 || j<n2; i++, j++){
+//             while(i<n1 && v1[i]!='.'){
+//                 x1=10*x1+(v1[i++]-'0');
+//             }
+//             while(j<n2 && v2[j]!='.'){
+//                 x2=10*x2+(v2[j++]-'0');
+//             }
+//             if (x1<x2) return -1;
+//             else if (x1>x2) return 1;
+//             x1=0;
+//             x2=0;
+//         }
+//         return 0;
+//     }
+// };
