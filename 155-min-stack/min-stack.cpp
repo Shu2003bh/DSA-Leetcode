a2c61725061d92efa09225ea340d707a086c2ebd
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2025-08-29 19:22:05 +0530

    Time: 3 ms (61.99%) | Memory: 23.4 MB (57.84%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,64 +1,74 @@
 class MinStack {
-    stack<long long> st; // Use long long to prevent overflow
-    long long mini;      // Use long long for minimum value
-    
 public:
+    long long mini;
+    stack<long long> st;
     MinStack() {
         mini = INT_MAX;
+        
     }
     
     void push(int val) {
-        long long value = val; // Convert to long long
-        
-        if(st.empty()) {
+        long long value = val;
+        if(st.empty()){
             st.push(value);
             mini = value;
         }
-        else {
-            if(value < mini) {
-                // Store the encoded value using long long to prevent overflow
-                st.push(2 * value - mini);
-                mini = value;
-            } else {
-                st.push(value);
-            }
+        else
+{
+        if(value<mini){
+            st.push(2*value-mini);
+            mini = value;
         }
+        else{
+            st.push(val);
+
+        }}
+        
     }
     
     void pop() {
-        if(st.empty()) {
-            return;
+        if(st.empty()){
+            return ;
         }
-        
-        long long top_val = st.top();
+        long long top = st.top();
         st.pop();
-        
-        if(top_val < mini) {
-            // Decode to find the previous minimum
-            long long prev_min = 2 * mini - top_val;
-            mini = prev_min;
+
+        if(top<mini){
+
+            mini = 2*mini - top;
         }
+        
     }
     
     int top() {
-        if(st.empty()) {
+        if(st.empty()){
             return -1;
         }
-        
-        long long top_val = st.top();
-        
-        if(top_val < mini) {
-            // If encoded value, return current minimum
+        long long top = st.top();
+
+        if(mini>top){
             return (int)mini;
-        } else {
-            return (int)top_val;
         }
+        else{
+            return (int)top;
+        }
+        
     }
     
     int getMin() {
-        if(st.empty()) {
+         if(st.empty()){
             return -1;
         }
         return (int)mini;
+        
     }
 };
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
