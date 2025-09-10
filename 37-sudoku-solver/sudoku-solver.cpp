f09890d9cf98e1f1a99d15aa6a2624b91eff6f19
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2025-09-10 15:40:27 +0530

    Time: 231 ms (68.95%) | Memory: 8.7 MB (89.31%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,60 +1,63 @@
 class Solution {
 public:
-
-    bool issafe(vector<vector<char>>& board,int row,int col,char digit){
+    bool issafe(int row,int col,vector<vector<char>>& board,char digit){
         for(int j = 0;j<9;j++){
-            if(board[row][j]==digit){
-                return false;
-            }
+           if( board[row][j]==digit){
+            return false;
+           }
         }
 
            for(int i = 0;i<9;i++){
-            if(board[i][col]==digit){
-                return false;
-            }
+           if( board[i][col]==digit){
+            return false;
+           }
         }
 
         int strow = (row/3)*3;
         int stcol = (col/3)*3;
 
-        for(int i =strow;i<=strow+2;i++){
-            for(int j =stcol;j<=stcol+2;j++){
+        for(int i = strow;i<=strow+2;i++){
+            for(int j = stcol;j<=stcol+2;j++){
                 if(board[i][j]==digit){
                     return false;
                 }
             }
         }
         return true;
+
     }
-    bool helper(vector<vector<char>>& board,int row,int col){
+    bool solve(vector<vector<char>>& board,int row,int col){
         if(row == 9){
-        return true;
+            return true;
         }
-        int nrow  = row,ncol=col+1;
-        if(ncol == 9){
-            nrow = row+ 1;
-            ncol = 0;
+        int strow = row;
+        int stcol = col+1;
+
+        if(stcol  == 9){
+            stcol =0;
+            strow +=1;
         }
 
         if(board[row][col]!='.'){
-           return helper(board,nrow,ncol);
+            return solve(board,strow,stcol);
         }
         else{
-            for(char dig = '1';dig<='9';dig++)
-            if(issafe(board,row,col,dig))
-                {
-                    board[row][col] = dig;
-                   if(helper(board,nrow,ncol)){
-                    return true;
-                   } 
-                    board[row][col] = '.';
+            for(char digit = '1';digit<='9';digit++){
+                if(issafe(row,col,board,digit)){
+                    board[row][col]=digit;
+                    if(solve(board,strow,stcol)){
+                        return true;
+                    }
+                    board[row][col]='.';
                 }
+
+            }
         }
         return false;
-
-
     }
     void solveSudoku(vector<vector<char>>& board) {
-       helper(board ,0,0);
+        solve(board,0,0);
+
+        
     }
 };
