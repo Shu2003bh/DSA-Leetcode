c83b679e6adaf9083deb925823244936e6bc0091
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2025-09-30 23:09:39 +0530

    Time: 4 ms (21.07%) | Memory: 17.4 MB (5.67%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -10,32 +10,31 @@
  * };
  */
 class Solution {
-private:
-    void solved(TreeNode* root, int lvl, int height, map<int, map<int, multiset<int>>> &mp) {
-        if(root == NULL) {
-            return;
-        }
-        
-        mp[lvl][height].insert(root->val);
-        solved(root->left, lvl-1, height+1, mp);
-        solved(root->right, lvl+1, height+1, mp);
-    }
-    
 public:
+    void solved(TreeNode* root,int lvl,int height,map<int,map<int,multiset<int>>> &mp){
+        if(root == NULL){
+            return ;
+        }
+        mp[lvl][height].insert(root->val);
+        solved(root->left,lvl-1,height+1,mp);
+        solved(root->right,lvl+1,height+1,mp);
+
+    }
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         vector<vector<int>> ans;
-        map<int, map<int, multiset<int>>> mp;
-        
-        solved(root, 0, 0, mp);
-        
-        for(auto i : mp) {
+        map<int,map<int,multiset<int>>> mp;
+        solved(root,0,0,mp);
+
+        for(auto i : mp){
             vector<int> col;
-            for(auto j : i.second) {
-                col.insert(col.end(), j.second.begin(), j.second.end());
+            for(auto j : i.second){
+                col.insert(col.end(),j.second.begin(),j.second.end());
             }
             ans.push_back(col);
         }
-        
+
         return ans;
+
+        
     }
 };
