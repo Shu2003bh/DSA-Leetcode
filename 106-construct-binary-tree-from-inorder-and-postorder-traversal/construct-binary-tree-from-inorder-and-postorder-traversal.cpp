44274d81c3c649987f1beaeee10a381d99978880
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2025-10-12 19:59:56 +0530

    Time: 7 ms (28.07%) | Memory: 26.9 MB (99.60%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -10,37 +10,35 @@
  * };
  */
 class Solution {
-    int find(vector<int>& inorder, int element, int n) {
-        for(int i = 0; i < n; i++) {
-            if(inorder[i] == element) {
+public:
+ int findpos(int idx,vector<int>& inorder,int n){
+        for(int i =0;i<n;i++){
+            if(inorder[i]==idx){
                 return i;
             }
         }
         return -1;
     }
     
-    TreeNode* solve(vector<int>& postorder, vector<int>& inorder, int& postIdx, int inStart, int inEnd, int n) {
-        if(postIdx < 0 || inStart > inEnd) {
+    TreeNode* solved(vector<int>& postorder, vector<int>& inorder,int &index,int st,int end,int n){
+        if(index<0 || st>end){
             return NULL;
         }
-        
-        int element = postorder[postIdx--];
+
+        int element = postorder[index--];
+        int pos = findpos(element,inorder,n);
         TreeNode* root = new TreeNode(element);
-        
-        int position = find(inorder, element, n);
-        
-        // Right subtree first (postorder property)
-        root->right = solve(postorder, inorder, postIdx, position + 1, inEnd, n);
-        // Then left subtree
-        root->left = solve(postorder, inorder, postIdx, inStart, position - 1, n);
-        
+        root->right = solved(postorder,inorder,index,pos+1,end,n);
+        root->left = solved(postorder,inorder,index,st,pos-1,n);
         return root;
     }
-    
-public:
+
+
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        int n = postorder.size();
-        int postIdx = n - 1;
-        return solve(postorder, inorder, postIdx, 0, n - 1, n);
+         int n = postorder.size();
+        int index = n-1;
+        TreeNode* ans = solved(postorder,inorder,index,0,n-1,n);
+        return ans;
+        
     }
 };
