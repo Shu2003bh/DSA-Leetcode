f826254fc08905161ebf1abea060d1fc0005fbb8
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2025-10-21 22:44:33 +0530

    Time: 1 ms (62.56%) | Memory: 39 MB (20.18%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -7,40 +7,35 @@
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ * };if
  */
 class Solution {
 public:
-    void solve(TreeNode* root,vector<int> &ans){
-        
-        if(root == NULL){
-            return ;
+    void solved(TreeNode* root,vector<int> &arr){
+        if(!root){
+            return;
         }
-        solve(root->left,ans);
-        ans.push_back(root->val);
-        solve(root->right,ans);
+        solved(root->left,arr);
+        arr.push_back(root->val);
+        solved(root->right,arr);
 
     }
     bool findTarget(TreeNode* root, int k) {
 
-        vector<int> ans;
-        solve(root,ans);
-        int i =0;
-        int j = ans.size()-1;
-        int sum =0;
-        while(i<j){
-             sum = ans[i] + ans[j];
-            if(sum > k){
-                j--;
-            }
-            else if(sum<k){
-                i++;
-            }
-            else{
+        vector<int> arr;
+        solved(root,arr);
+        // sort(arr.begin(),arr.end());
+
+        unordered_map<int,int> mp;
+        for(int i =0;i<arr.size();i++){
+            int c = k - arr[i];
+            if(mp.find(c)!=mp.end()){
                 return true;
             }
+            mp[arr[i]] = i;
         }
-       return false;
+        return false;
+
         
     }
 };
