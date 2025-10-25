cceef750a7a2d403172e9b680d2ae14e690401cf
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2025-10-25 22:15:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (13.49%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int cnt = 0;
-        int sum  = 0;
+        int week = 0;
+        int money  = 0;
         while(n>0){
             if(n>=7){
                 for(int i =1;i<=7;i++){
-                    sum = sum + cnt + i; 
+                    money = money + week + i; 
                 }
             }
             else{
                 for(int i =1;i<=n;i++){
-                    sum = sum + cnt + i;
+                    money = money + week + i;
                 }
             }
 
-            cnt++;
+            week++;
             n = n-7;
         }
-        return sum ;
+        return money ;
     }
 };
