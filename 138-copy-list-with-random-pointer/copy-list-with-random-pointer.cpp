31cb7599d0649e042d02dc3cc34333526e4a0a22
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2025-12-05 23:33:10 +0530

    Time: 6 ms (55.60%) | Memory: 15.1 MB (49.57%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -20,15 +20,15 @@ public:
         unordered_map<Node*,Node*> mp;
         Node* curr = head;
         while(curr){
-        mp[curr] = new Node(curr->val);
-        curr= curr->next;}
-
+            mp[curr] = new Node(curr->val);
+            curr= curr->next;
+        }
 
         curr = head;
         while(curr){
-        mp[curr]->next = mp[curr->next];
-        mp[curr]->random = mp[curr->random];
-        curr= curr->next;
+            mp[curr]->next = mp[curr->next];
+            mp[curr]->random = mp[curr->random];
+            curr = curr->next;
         }
 
         return mp[head];
