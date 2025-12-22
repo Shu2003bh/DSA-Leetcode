5661fcfff130718d6dda75af9bbec2b148da50bd
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2025-12-22 22:35:35 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (75.05%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -12,15 +12,15 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-   
+
         if(root == NULL){
             return 0;
         }
+
         int left = maxDepth(root->left);
         int right = maxDepth(root->right);
-        int cnt =max(left,right)+1;
 
-        return cnt;
+        return 1+max(left,right);
         
     }
 };
