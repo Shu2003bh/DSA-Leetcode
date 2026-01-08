1874b7d90928357609f889b9f0904d746dbe3983
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2026-01-08 15:54:54 +0530

    Time: 3 ms (69.48%) | Memory: 32.1 MB (33.74%) - LeetSync

--- 173-binary-search-tree-iterator/binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.cpp
@@ -1,25 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class BSTIterator {
 public:
-    stack<TreeNode*> s;
-    BSTIterator(TreeNode* root) {
-        partialInorder(root);
-    }
-    
-    void partialInorder(TreeNode* root){
-        while(root != NULL){
-            s.push(root);
-            root = root->left;
+vector<int> arr;
+int i = 0;
+
+    void solve(TreeNode* root){
+        if(root == NULL){
+            return;
         }
+        if(root->left){
+            solve(root->left);
+
+        }
+        arr.push_back(root->val);
+        if(root->right){
+            solve(root->right);
+        }
+    }
+    BSTIterator(TreeNode* root) {
+       solve(root);
+        
     }
     
     int next() {
-        TreeNode* top = s.top();
-        s.pop();
-        partialInorder(top->right);
-        return top->val;
+        int n = arr.size();
+        if(i<n){
+            return arr[i++];
+
+        }
+        return -1;
+        
     }
     
     bool hasNext() {
-        return !s.empty();
+        int n = arr.size();
+        if(i<n){
+            return true;
+        }
+        else
+        return false;
+        
     }
 };
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
