329e4f1918d69ad245fda9afbed95385eac70129
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2026-01-18 20:54:39 +0530

    Time: 3 ms (63.43%) | Memory: 12.6 MB (13.84%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,76 +1,87 @@
 class Solution {
 public:
-    void addsol(int n,vector<vector<string>> &ans,vector<vector<bool>> &vis){
-        vector<string> temp;
-        for(int i =0;i<n;i++){
-            string s;
-            for(int j =0;j<n;j++){
-                if(vis[i][j]==1){
-                    s+= "Q";
-                }
-                else{
-                    s += ".";
-                }
-
+void addsol(int n,vector<vector<string>>&ans,vector<vector<bool>> vis){
+    vector<string> temp1;
+    for(int i = 0;i<n;i++){
+        string temp;
+        for(int j =0;j<n;j++){
+            if(vis[i][j]==1){
+                temp +='Q';
             }
-            temp.push_back(s);
-        }
-        ans.push_back(temp);
+                else{
+                    temp += '.';
+                }
+            
 
+
+        }
+        temp1.push_back(temp);
     }
-    bool issafe(int row ,int col,vector<vector<bool>> &vis,int n){
-        int x = row ;
+    ans.push_back(temp1);
+}
+ bool issafe(int row,int col,vector<vector<bool>>& vis,int n){
+        int x = row;
         int y = col;
         while(y>=0){
             if(vis[x][y]==1){
                 return false;
             }
             y--;
-
         }
+
         x = row;
         y = col;
-
-        while(x>=0 && y>=0){
-            if(vis[x][y]==1){
+         while( y>=0 && x>=0){
+             if(vis[x][y]==1){
                 return false;
             }
-            y--;
             x--;
-        }
+            y--;
 
-        x = row;
-        y = col;
-        while(x<n && y>=0){
-            if(vis[x][y]==1){
+         }
+
+         x = row;
+         y = col;
+
+         while(x<n && y>=0){
+              if(vis[x][y]==1){
                 return false;
             }
             x++;
             y--;
-        }
-        return true;
+         }
+         return true;
+
 
     }
-    void solve(int col,int n,vector<vector<string>> &ans,vector<vector<bool>> &vis){
+
+    void solve(int col,int n,vector<vector<string>>&ans,vector<vector<bool>> &vis){
         if(col == n){
             addsol(n,ans,vis);
-            return ;
+            return;
         }
 
-        for( int row = 0;row<n;row++){
+
+        for(int row =0;row<n;row++){
             if(issafe(row,col,vis,n)){
                 vis[row][col]=1;
                 solve(col+1,n,ans,vis);
                 vis[row][col]=0;
+
+
             }
+
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
-        vector<vector<bool>> vis(n,vector<bool>(n,0));
+         if(n==0){
+            return ans;
+        }
+        
+        vector<vector<bool>> vis(n,vector<bool>(n,false));
         solve(0,n,ans,vis);
         return ans;
-
         
     }
 };
