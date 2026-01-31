81bff98c04fadd51bd8225f4ef967e2af3103060
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2026-02-01 00:57:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (49.97%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int i = 1;
+        // int i = 1;
         int cnt = 0;
-        while(n/pow(5,i)>0){
-            cnt += n/pow(5,i);
-            // n = n/pow(5,i);
-            i++;
+        // while(n/pow(5,i)>0){
+        //     cnt += n/pow(5,i);
+        //     i++;
+        // }
+
+        while(n>0){
+            n = n/5;
+            cnt += n;
         }
         return cnt ;
         
