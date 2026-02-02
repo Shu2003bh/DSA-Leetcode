826810ff1791d16c211e7705208ae733ff306ccb
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2026-02-02 23:53:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (43.45%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
 
-    void power(double x, int n,double ans){
-        if(n == 0){
-            return;
-        }
-        ans *=x;
-        power(x,n-1,ans);
-     
-
-    }
 
     double myPow(double x, int n) {
-        if(x == 0){
-            return 0.0;
-        }
-        if(n == 0){
-            return 1.0;
-        }
-        double ans = x;
-        return pow(x,n);
-        
+        long long N = n;
+       if(N<0){
+        N = -N;
+        x = 1/x;
+       }
+        double ans = 1;
+       while(N>0){
+        if(N%2==1)
+        ans *= x;
+
+        x*=x;
+
+    N /= 2;;
+       }
+        return ans;
     }
 };
