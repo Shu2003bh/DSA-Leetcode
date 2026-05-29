3fe2408561071526dd71b2e65ec98f93cc6649ae
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2026-05-29 14:17:23 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (24.99%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -1,61 +1,38 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    int solve(ListNode* head){
-      
+    int Nodecnt(ListNode* head){
         int cnt = 0;
-        while(head!=NULL){
+        while(head->next!=NULL){
             head = head->next;
             cnt++;
         }
         return cnt;
     }
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(head == NULL ){
+        if(n==1 && head->next ==  NULL){
             return NULL;
         }
 
-        int cnt = solve(head);
-    if(cnt == n){
-        if(head->next == NULL){
-            return NULL;
+        int cnt = Nodecnt(head);
+        int rem = cnt - n +1;
+        if(rem == 0){
+            return head->next;
         }
-        ListNode* temp = head->next;
-        delete(head);
-        return temp;
-    }
 
-    
+        ListNode* prev = NULL;
+        ListNode* next = head;
 
-        int num = cnt-n;
-        int i =0;
-        ListNode* curr = head;
-        while(i!=num-1){
-            curr = curr->next;
-            i++;
 
+        while(rem){
+           prev = next;
+           next = next->next;
+           rem--;
         }
-        ListNode* temp = curr->next;
-        if(curr->next!=NULL){
-        curr->next = temp->next;
-        temp->next = NULL;
-        }
-        else{
-            curr->next = NULL;
-        }
-        delete(temp);
+
+        prev->next = next->next;
+        next->next = NULL;
+
         return head;
-
-
 
         
     }
