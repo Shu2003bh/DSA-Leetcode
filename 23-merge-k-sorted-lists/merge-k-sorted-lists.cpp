0f4a86b9bab028abb616ae997e9b691546526799
SHUBHAM BAJPAI <145864470+Shu2003bh@users.noreply.github.com>
2026-06-01 23:42:23 +0530

    Time: 155 ms (6.69%) | Memory: 18.6 MB (43.69%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,73 +1,31 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-
-    ListNode* merged(ListNode* l1,ListNode* l2){
-        if(!l1){
-            return l2;
-
+    ListNode* mergeTwoNode(ListNode* l1,ListNode* l2){
+        if(l1==NULL || l2==NULL){
+            return l1 == NULL ? l2 : l1;
         }
-        if(!l2){
+
+        if(l1->val>=l2->val){
+            l2->next = mergeTwoNode(l1,l2->next);
+            return l2;
+        }
+        else{
+            l1->next = mergeTwoNode(l1->next,l2);
             return l1;
         }
-        ListNode* dummy = new ListNode(-1);
-        ListNode* curr = dummy;
-
-        while(l1&&l2){
-            if(l1->val > l2->val){
-                curr->next = l2;
-                l2 = l2->next;
-            }
-            else{
-                curr->next = l1;
-                l1 = l1->next;
-            }
-            curr = curr->next;
-        }
-
-        while(l1){
-            curr->next = l1;
-            l1 = l1->next;
-            curr = curr->next;
-        }
-
-        while(l2){
-            curr->next = l2;
-            l2 = l2->next;
-            curr = curr->next;
-        }
-
-        return dummy->next;
-    }
-
-    ListNode* solve(int left,int right,vector<ListNode*>& lists){
-        if(left>right){
-            return NULL;
-        }
-        if(left == right){
-    return lists[left];
-}
-
-
-        int mid = (left + right)/2;
-        ListNode*l1 = solve(left,mid,lists);
-        ListNode*l2 = solve(mid+1,right,lists);
-        return merged(l1,l2);
     }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if(lists.size()==0){
-            return NULL;      
-              }
-              return solve(0,lists.size()-1,lists);
+            return NULL;
+        }
+        if(lists.size()==1){
+            return lists[0];
+        }
+        ListNode* head = lists[0];
+        for(int i =1;i<lists.size();i++){
+            head = mergeTwoNode(head,lists[i]);
+        }
+        return head;
         
     }
 };
